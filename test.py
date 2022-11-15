cffc9608c9e75460d434bfb96f7710093d498b1e
Alleycat404 <Rioland@126.com>
2022-11-15 11:17:21 +0800

    mkdir checkpoint

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import torch
-# from google import ScaleHyperprior
+from compressai.models.google import ScaleHyperprior
 
-model = torch.load('checkpoint_best_loss.pth.tar')
+# model = torch.load('checkpoint_best_loss.pth.tar')
 
-for k, v in model['state_dict'].items():
-  print(k)
+# for k, v in model['state_dict'].items():
+#   print(k)
 
-# mod = ScaleHyperprior()
-# mod.load_state_dict(model)
+mod = ScaleHyperprior()
+print(mod)
