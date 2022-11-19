4e92afcac04c7d1b4be814c8349cabb9497dcd2d
Alleycat404 <Rioland@126.com>
2022-11-19 20:24:11 +0800

    google.py M=512 N=128

--- compressai/models/google.py
+++ compressai/models/google.py
@@ -622,8 +622,8 @@ class JointAutoregressiveHierarchicalPriors(MeanScaleHyperprior):
     @classmethod
     def from_state_dict(cls, state_dict):
         """Return a new model instance from `state_dict`."""
-        N = state_dict["g_a.0.weight"].size(0)
-        M = state_dict["g_a.6.weight"].size(0)
+        N = 128
+        M = 512
         net = cls(N, M)
         net.load_state_dict(state_dict)
         return net
